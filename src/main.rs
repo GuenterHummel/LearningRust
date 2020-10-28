2157e24854c97b195815d5120861b1117ff2c2ec
Günter Hummel <guenter.hummel@sbs.co.at>
2020-10-28 13:54:56 +0100

    Added an function with UTF8 supprort

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,20 @@ fn main() {
     println!("Hello, world!");
     println!("Gue was here");
     println!("Printing {} lines", "some");
+
+    greet_world();
 }
+
+fn greet_world() {
+    println!("Hello, world!");
+
+    let southern_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+
+    let regions = [southern_germany, japan];
+
+    for region in regions.iter() {
+            println!("{}", &region);
+    }
+}
+

dbec14693a285bbd4faf4587749cc2a43d3bbef3
Günter Hummel <guenter.hummel@sbs.co.at>
2023-04-18 16:49:51 +0200

    fixed some spaceing

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -5,7 +5,7 @@ use std::env;
 
 fn gcd(mut n: u64, mut m: u64) -> u64 { 
     assert!(n != 0 && m != 0); 
-    while m !=0 {
+    while m != 0 {
         if m < n { 
             std::mem::swap(&mut m, &mut n);
         }
